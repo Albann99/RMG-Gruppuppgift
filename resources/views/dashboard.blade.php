08e631fb98781065c3f4720a6e107dcde8f14406
Sargon <Sargonpoli@hotmail.com>
2019-03-07 13:36:16 +0100

    Create dashboard.blade.php

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts/app')
+
+@section('content')
+    <div class="container mt-3">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Dashboard</div>
+
+                <p>You are logged in!</p>
+
+                    <p>This is the dashboard.</p>
+
+                    <a href="articles/create">Create new Article</a>
+@endsection
+                </div>
+            </div>
+        </div>
+    </div>
