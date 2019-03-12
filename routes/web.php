7b444e13cc7fb34e14ae5472f0e298d4bbb606f0
Alban <kingenalban@gmail.com>
2019-03-12 14:11:59 +0100

    routes

--- routes/web.php
+++ routes/web.php
@@ -11,14 +11,31 @@
 |
 */
 
+Auth::routes();
+
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('category', function () {
-    return view('category');
+
+Route::get('/bookings/thankyou', function () {
+	return view('bookings/thankyou');
 });
 
-Auth::routes();
+Route::resource('/bookings', 'BookingController');
+
+Route::middleware(['auth'])->group(function() { 
+Route::get('/articles/myarticles', function () {
+	return view('/articles/myarticles');
+});
+});
+
+Route::resource('/categories', 'CategoryController');
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::resource('/articles', 'ArticleController');
+Route::post('/bookings', 'BookingController@store');
+
+Route::middleware(['auth'])->group(function() { 
+Route::get('/dashboard', 'DashboardController@index');
+});
