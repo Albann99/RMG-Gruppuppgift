fa2677ad1056ab768c946693137f26f8e09a8d71
Sargon <Sargonpoli@hotmail.com>
2019-03-13 13:13:09 +0100

    view/dashboard
    sargon

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,11 +2,15 @@
 
 namespace App\Http\Controllers;
 
+
+
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function index() {
+    	
+    	
     	return view('dashboard');
     }
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -12,6 +12,25 @@
 					<p>This is the dashboard.</p>
 
 					<a href="articles/create">Create new Article</a>
+					<a href="articles/myarticles">My Articles</a>
+					<br>
+				
+				@php
+use App\Booking;
+@endphp
+				@if ($bookings = Booking::where('article_id', auth()->id())->get())
+					<p>Bokningsförfrågningar:</p>
+					
+		<ol>
+		@foreach($bookings as $booking)
+		<li><a href="/bookings/{{$booking->id }}">{{$booking->firstname }}</a></li>
+		@endforeach
+		@endif
+	</ol>
+
+
+
+					
 @endsection
 				</div>
 			</div>
