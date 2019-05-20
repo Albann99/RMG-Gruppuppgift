8e14d61fd4fe2cf236c52be30478a1cadb91cc6f
Alban <kingenalban@gmail.com>
2019-05-20 15:38:26 +0200

    Delete thankyou.blade.php

--- resources/views/bookings/thankyou.blade.php
+++
@@ -1,9 +0,0 @@
-
-		@extends('layouts.app')
-
-@section('content')
-
-<meta http-equiv="refresh" content="3;url=http://127.0.0.1:8000/articles" />
-<h1>Thank you for your booking</h1>
-
-@endsection
